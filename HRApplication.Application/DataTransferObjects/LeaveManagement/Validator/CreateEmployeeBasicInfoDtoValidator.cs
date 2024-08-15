8eb4cfb11cc0c4498ee06dd4c2d4da2647b99470
senhimadri <senhimadri.dev@gmail.com>
2024-08-15 17:41:43 +0600

    FluentValidation Added

--- HRApplication.Application/DataTransferObjects/LeaveManagement/Validator/CreateEmployeeBasicInfoDtoValidator.cs
+++ HRApplication.Application/DataTransferObjects/LeaveManagement/Validator/CreateEmployeeBasicInfoDtoValidator.cs
@@ -1,14 +1,34 @@
 using FluentValidation;
+using HRApplication.Application.Contracts.Parsistence;
+using HRApplication.Application.Contracts.Parsistence.EmployeeManagement;
 
 namespace HRApplication.Application.DataTransferObjects.LeaveManagement.Validator;
 
 public class CreateEmployeeBasicInfoDtoValidator : AbstractValidator<CreateEmployeeBasicInfoDto>
 {
-	public CreateEmployeeBasicInfoDtoValidator()
+	private readonly IUnitofWork _unitOfWork ;
+	public CreateEmployeeBasicInfoDtoValidator(IUnitofWork unitOfWork)
 	{
-		RuleFor(x => x.StrEmployeeName)
-			.Length(0, 100).WithErrorCode("{PropertyName} ");
-	}
+		_unitOfWork = unitOfWork;
+
+        RuleFor(x => x.StrEmployeeName)
+			.NotEmpty().NotNull().WithMessage("{PropertyName} shouldn't be empty")
+			.Length(0, 100).WithMessage("Employee Name shouldn't be more than 100");
+
+		RuleFor(x => x.StrEmployeeCode)
+            .NotEmpty().NotNull().WithMessage("{PropertyName} shouldn't be empty")
+            .Length(0, 50).WithMessage("Employee Code shouldn't be more than 50")
+			.MustAsync(async (empCode, token) =>
+			{
+				var employeeCodeExit = await _unitOfWork
+						.EmployeeBasicInfoRepository
+						.GetCount(x => x.StrEmployeeCode== empCode);
+				return employeeCodeExit > 0;
+			}).WithMessage("Employee Code is already exit");
+
+		RuleFor(x => x.DteDateOfBirth)
+			.NotNull().NotEmpty().WithMessage("{PropertyName} shouldn't be empty");
+    }
 }
 
 
