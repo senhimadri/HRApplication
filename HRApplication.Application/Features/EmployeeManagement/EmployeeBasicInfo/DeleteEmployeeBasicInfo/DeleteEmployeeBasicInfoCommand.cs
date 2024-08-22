efe7f700d9e63b5ee6aa15c74bce24f80a4808e5
senhimadri <senhimadri.dev@gmail.com>
2024-08-22 15:20:28 +0600

    New Update Update Employee

---
+++ HRApplication.Application/Features/EmployeeManagement/EmployeeBasicInfo/DeleteEmployeeBasicInfo/DeleteEmployeeBasicInfoCommand.cs
@@ -0,0 +1,7 @@
+namespace HRApplication.Application.Features.EmployeeManagement.EmployeeBasicInfo.DeleteEmployeeBasicInfo
+{
+    public class DeleteEmployeeBasicInfoCommand
+    {
+        // Class implementation
+    }
+}
---
+++ HRApplication.Application/Features/EmployeeManagement/EmployeeBasicInfo/DeleteEmployeeBasicInfo/DeleteEmployeeBasicInfoCommandHandler.cs
@@ -0,0 +1,7 @@
+namespace HRApplication.Application.Features.EmployeeManagement.EmployeeBasicInfo.DeleteEmployeeBasicInfo
+{
+    public class DeleteEmployeeBasicInfoCommandHandler
+    {
+        // Class implementation
+    }
+}
