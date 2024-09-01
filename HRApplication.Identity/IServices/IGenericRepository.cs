f9164a69bf10dcf5fa2504439381f4e76734bce1
senhimadri <senhimadri.dev@gmail.com>
2024-09-01 14:51:35 +0600

    feat: Interface Added

--- HRApplication.Identity/IServices/IGenericRepository.cs
+++ HRApplication.Identity/IServices/IGenericRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace HRApplication.Identity.IServices;
+﻿using System.Linq.Expressions;
+
+namespace HRApplication.Identity.IServices;
 
 public interface IGenericRepository<T> where T : class
 {
-
+    Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+    Task<T> GetByIdAsync(Guid id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(Guid id);
+    Task<bool> IsExist(Expression<Func<T, bool>> filter);
+    Task<long> GetCount(Expression<Func<T, bool>> filter);
 }
