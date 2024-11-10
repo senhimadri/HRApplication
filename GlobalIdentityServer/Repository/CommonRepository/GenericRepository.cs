39c617dd70ac212a54518c6e7200cc1eadf3fd4e
senhimadri <senhimadri.dev@gmail.com>
2024-11-10 12:40:11 +0600

    fix : Little Format Fix

--- GlobalIdentityServer/Repository/CommonRepository/GenericRepository.cs
+++ GlobalIdentityServer/Repository/CommonRepository/GenericRepository.cs
@@ -4,41 +4,27 @@ using System.Linq.Expressions;
 
 namespace GlobalIdentityServer.Repository.CommonRepository;
 
-public class GenericRepository<T> : IGenericRepository<T> where T : BaseDomain
+public class GenericRepository<T>(IMongoDatabase database, string collectionName) 
+                                                        : IGenericRepository<T> where T : BaseDomain
 {
-    private readonly IMongoCollection<T> _context;
+    private readonly IMongoCollection<T> _context = database.GetCollection<T>(collectionName);
     private readonly FilterDefinitionBuilder<T> filterBuilder = Builders<T>.Filter;
 
-    public GenericRepository(IMongoDatabase database, string collectionName)
-                            => _context = database.GetCollection<T>(collectionName);
+    public async Task CreateAsync(T entity) 
+                                    => await _context.InsertOneAsync(entity);
 
-    public async Task CreateAsync(T entity)
-    {
-        await _context.InsertOneAsync(entity);
-    }
+    public async Task<IReadOnlyCollection<T>> GetAllAsync() 
+                                    => await _context.Find(filterBuilder.Empty).ToListAsync();
 
-    public async Task<IReadOnlyCollection<T>> GetAllAsync()
-    {
-        return await _context.Find(filterBuilder.Empty).ToListAsync();
-    }
-
-    public async Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter)
-    {
-        return await _context.Find(filter).ToListAsync();
-    }
+    public async Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter) 
+                                    => await _context.Find(filter).ToListAsync();
 
     public async Task<T> GetAsync(Guid id)
-    {
-        return await _context.Find(x => x.Id == id).FirstOrDefaultAsync();
-    }
+                                    => await _context.Find(x => x.Id == id).FirstOrDefaultAsync();
 
     public async Task UpdateAsync(T entity)
-    {
-        await _context.ReplaceOneAsync(filter: x => x.Id == entity.Id , replacement: entity);
-    }
+                                    => await _context.ReplaceOneAsync(filter: x => x.Id == entity.Id, replacement: entity);
 
     public async Task RemoveAsync(Guid id)
-    {
-        await _context.DeleteOneAsync(filter: x => x.Id == id);
-    }
+                                    => await _context.DeleteOneAsync(filter: x => x.Id == id);
 }
